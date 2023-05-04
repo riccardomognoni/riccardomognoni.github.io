098d03735b07b3abb34dc8d0aaa3b1991ed268f9
Riccardo Mognoni <116260265+riccardomognoni@users.noreply.github.com>
2023-05-04 09:38:07 +0200

    Update Campo.js

--- campominato/scripts/Campo.js
+++ campominato/scripts/Campo.js
@@ -134,9 +134,7 @@ class Campo {
             if(this.campo[r][c].mina && this.campo[r][c].flag){
                 this.mineScoperte++;
             }
-            else if(!this.campo[r][c].flag){
-                  this.mineScoperte--;
-            }
+            
 
 
 
